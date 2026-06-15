a506addbc1d9d47de345733816b815d0270ee94f
Kingewor9 <michealewor9@gmail.com>
2026-06-15 22:15:45 +0100

    Made a fix to the recent keyword

--- config.py
+++ config.py
@@ -45,13 +45,13 @@ KEYWORD_REPLIES = {
         "I'm good, thank you"
     ),
     "Offer":(
-        "Congratulations on showing interest for the DFY Tiktok Ads Offer n\n"
-        "Kindly transfer the sum of 50k to this account below n\n"
-        "Account Name: Tikmarketinghub n\n"
-        "Account Number: 3003247241 n\n"
-        "Bank Name: Kuda MFB n\n"
+        "Congratulations on showing interest for the DFY Tiktok Ads Offer \n\n"
+        "Kindly transfer the sum of 50k to this account below \n\n"
+        "Account Name: Tikmarketinghub \n\n"
+        "Account Number: 3003247241 \n\n"
+        "Bank Name: Kuda MFB \n\n"
         "And send your payment receipts afterwards."
-    )
+    ),
 
     # ---- Add more keywords below this line ----
     # "Price": "Here is our pricing list: ...",
